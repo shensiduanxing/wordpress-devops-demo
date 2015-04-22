1713624b25bad27cf74ecc1292f348564804a860
mihoyo <devops@mihoyo.com>
2015-04-23 07:59:24 +0800

    modify install page title.

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -214,7 +214,7 @@ switch($step) {
 
 		display_header();
 ?>
-<h1><?php _ex( 'Welcome to Fit2Cloud Wordpress', 'Howdy' ); ?></h1>
+<h1><?php _ex( 'Welcome to DevOps Demo Project Wordpress', 'Howdy' ); ?></h1>
 <p><?php _e( 'Welcome to the famous five-minute WordPress installation process! Just fill in the information below and you&#8217;ll be on your way to using the most extendable and powerful personal publishing platform in the world.' ); ?></p>
 
 <h1><?php _e( 'Information needed' ); ?></h1>
